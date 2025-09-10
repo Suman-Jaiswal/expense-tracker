c7b5d18037605f0056dfcb10b026cab04b29f6ff
s0j0b3x <suman.jaiswal@walmart.com>
2025-09-10 21:28:22 +0530

    .

--- server/server.js
+++ server/server.js
@@ -1,10 +1,7 @@
-import bodyParser from "body-parser";
 import express from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Server is running");
